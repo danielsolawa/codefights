b8f0ca703b78d7bee8a7adecf7ab475cac47f569
Daniel Solawa <danielsolawa@gmail.com>
2018-05-07 11:03:26 +0200

    LargestNumber

---
+++ src/com/danielsolawa/codefights/core/solutions/LargestNumber.java
@@ -0,0 +1,13 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class LargestNumber {
+	
+	int largestNumber(int n) {
+		String largest = "";
+		for(int i = 1; i <= n; i++) {
+			largest += 9;
+		}
+		
+		return Integer.parseInt(largest);
+	}
+}
