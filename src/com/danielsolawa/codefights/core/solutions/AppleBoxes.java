dba9c28ffff0a9e74a911910479498e0f99a3b9c
Daniel Solawa <danielsolawa@gmail.com>
2018-05-15 12:47:03 +0200

    AppleBoxes

---
+++ src/com/danielsolawa/codefights/core/solutions/AppleBoxes.java
@@ -0,0 +1,19 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class AppleBoxes {
+
+	int appleBoxes(int k) {
+		int yellow = 0;
+		int red = 0;
+		for(int i = 1; i <= k; i++) {
+			if((i & 1) == 0)
+				red += (i*i);
+			else
+				yellow += (i*i);	
+		}
+
+		return red - yellow;
+	
+	}
+	
+}
