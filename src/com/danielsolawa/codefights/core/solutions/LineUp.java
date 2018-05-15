59fcc084d85a3dbfdea03afb1eab5f20baabc2b2
Daniel Solawa <danielsolawa@gmail.com>
2018-05-15 11:54:38 +0200

    LineUp

---
+++ src/com/danielsolawa/codefights/core/solutions/LineUp.java
@@ -0,0 +1,24 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class LineUp {
+
+	int lineUp(String commands) {
+		int x = 0;
+		int count = 0;
+		for (int i = 0; i < commands.length(); i++) {
+			char currentLetter = commands.charAt(i);
+			if(currentLetter == 'A')
+				x += 2;
+			else if(currentLetter == 'L')
+				x += 1;
+			else
+				x -= 1;
+			
+			count = (x % 2 == 0) ? count + 1 : count;
+		}
+		
+		return count;
+	}
+	
+	
+}
