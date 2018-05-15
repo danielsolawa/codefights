375257b2a3df81c65eef9e2167a3afbe766cf294
Daniel Solawa <danielsolawa@gmail.com>
2018-05-15 13:23:03 +0200

    AppleBoxes Recursive Solution

--- src/com/danielsolawa/codefights/core/solutions/AppleBoxes.java
+++ src/com/danielsolawa/codefights/core/solutions/AppleBoxes.java
@@ -2,6 +2,9 @@ package com.danielsolawa.codefights.core.solutions;
 
 public class AppleBoxes {
 
+	/*
+	 * Iterative Solution
+	 */
 	int appleBoxes(int k) {
 		int yellow = 0;
 		int red = 0;
@@ -14,6 +17,18 @@ public class AppleBoxes {
 
 		return red - yellow;
 	
+	}
+	
+	/*
+	 * Recursive Solution
+	 */
+	static int appleBoxesRec(int k) {
+		
+		return k % 2 == 0 ? countApples(k) - countApples(k-1) : countApples(k-1) - countApples(k);
+	}
+	
+	static int countApples(int x) {
+		return x > 0 ? (x * x) + countApples(x -2) : 0;
 	}
 	
 }
