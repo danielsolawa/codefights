1e6fdf1d2054fd8ea734b91a726d67974b1d27de
Daniel Solawa <danielsolawa@gmail.com>
2018-05-16 15:52:30 +0200

    ArrayReplace

---
+++ src/com/danielsolawa/codefights/core/solutions/ArrayReplace.java
@@ -0,0 +1,11 @@
+package com.danielsolawa.codefights.core.solutions;
+
+import java.util.Arrays;
+
+public class ArrayReplace {
+
+	int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+		return Arrays.stream(inputArray).map(n -> n == elemToReplace ? substitutionElem : n).toArray();
+	}
+	
+}
