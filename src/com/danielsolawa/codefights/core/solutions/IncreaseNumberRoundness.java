85d87d657db6f32e7ea230c1ac22040085004c7c
Daniel Solawa <danielsolawa@gmail.com>
2018-05-16 09:40:26 +0200

    IncreaseNumberRoundness

---
+++ src/com/danielsolawa/codefights/core/solutions/IncreaseNumberRoundness.java
@@ -0,0 +1,21 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class IncreaseNumberRoundness {
+
+	boolean increaseNumberRoundness(int n) {
+		String s = String.valueOf(n);
+		char lastDigit = s.charAt(s.length()-1);
+		for(int i = s.length() -2; i >= 0; i--) {
+			
+			if(lastDigit != '0' && s.charAt(i) == '0') {
+				return true;
+			}
+				
+			lastDigit = s.charAt(i);
+		}
+		
+		return false;
+	}
+	
+	
+}
