f2e40acaa14c39b3cd7bf00649b7c2e2d1865fc0
Daniel Solawa <danielsolawa@gmail.com>
2018-05-16 16:08:45 +0200

    FirstReverseTry refactor

--- src/com/danielsolawa/codefights/core/solutions/FirstReverseTry.java
+++ src/com/danielsolawa/codefights/core/solutions/FirstReverseTry.java
@@ -2,4 +2,16 @@ package com.danielsolawa.codefights.core.solutions;
 
 public class FirstReverseTry {
 
+	int[] firstReverseTry(int[] arr) {
+		if(arr.length == 0)
+			return arr;
+		
+		arr[0] = arr[0] + arr[arr.length-1];
+		arr[arr.length-1] = arr[0] - arr[arr.length-1];
+		arr[0] = arr[0] - arr[arr.length-1];
+		
+		return arr;
+				
+	}
+	
 }
