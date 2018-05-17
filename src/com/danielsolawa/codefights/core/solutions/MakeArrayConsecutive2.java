396656370b549dc8f35477e45a81bef4473ee30d
Daniel Solawa <danielsolawa@gmail.com>
2018-05-17 12:26:49 +0200

    MakeArrayConsecutive2

---
+++ src/com/danielsolawa/codefights/core/solutions/MakeArrayConsecutive2.java
@@ -0,0 +1,14 @@
+package com.danielsolawa.codefights.core.solutions;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MakeArrayConsecutive2 {
+
+	int makeArrayConsecutive2(int[] statues) {
+		Arrays.sort(statues);
+		return (int) IntStream.range(1, statues.length).map(n -> Math.abs(statues[n] - statues[n-1] -1)).sum();
+	}
+	
+	
+}
