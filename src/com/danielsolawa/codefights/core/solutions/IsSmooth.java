3f034f0307d360fa592b70c155afab5f70802381
Daniel Solawa <danielsolawa@gmail.com>
2018-05-17 11:57:57 +0200

    IsSmooth

---
+++ src/com/danielsolawa/codefights/core/solutions/IsSmooth.java
@@ -0,0 +1,23 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class IsSmooth {
+
+	boolean isSmooth(int[] arr) {
+		int middle = 0;
+		int index = 0;
+		if((arr.length & 1) == 0) {
+			index = (int) Math.ceil((arr.length-1)/2);	
+			middle = arr[index] + arr[index+1];
+		}
+		else {
+			index = (arr.length-1)/2;
+			middle = arr[index];
+		}
+
+		
+		return arr[0] == middle && middle == arr[arr.length-1];
+		
+	}
+	
+	
+}
