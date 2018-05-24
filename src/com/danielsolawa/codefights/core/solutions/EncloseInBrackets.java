ab493f18df8d09e892ccc8785ae5852d6ced3324
Daniel Solawa <danielsolawa@gmail.com>
2018-05-24 10:58:13 +0200

    EncloseInBrackets

---
+++ src/com/danielsolawa/codefights/core/solutions/EncloseInBrackets.java
@@ -0,0 +1,10 @@
+package com.danielsolawa.codefights.core.solutions;
+
+public class EncloseInBrackets {
+
+	String encloseInBrackets(String inputString) {
+
+		return "("+ inputString + ")";
+	}
+	
+}
